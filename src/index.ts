a22d18882e02a60c36707c74f9aaf13a2359c1f3
eduardor0cha <c.eduardo_rn@outlook.com>
2023-05-31 12:28:56 -0300

    feat: babel and redis setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import app from "./server";
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
+import "dotenv/config";
 
 app.listen(process.env.PORT);
 
--- src/redis.ts
+++ src/redis.ts
@@ -1,5 +1,13 @@
+import "dotenv/config";
 import { createClient } from "redis";
-const rClient = createClient();
+
+const rClient = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOSTNAME,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
 
 async function initRedis() {
   await rClient.connect();
